ab8c4e08edfb6f3c0dcb98d10b4822bc89c8e64e
zenin5479 <zenin5479@gmail.com>
2025-10-27 14:08:20 +0300

    Update 10.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -184,7 +184,6 @@ namespace FunctionsPartTwo
          Console.WriteLine("Определение наличия в массиве чисел заданной последовательности");
          Console.WriteLine("---------------------------------------------------------------");
          Random desultory = new Random();
-
          int numberH = desultory.Next(100, 998);
          Console.WriteLine("Введите последовательность для поиска: " + numberH);
          int[] arrayy = new int[15];
