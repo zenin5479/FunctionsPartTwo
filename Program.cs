26a123c61d23253bb042ec16de8f5ea463264a7f
zenin5479 <zenin5479@gmail.com>
2025-10-29 15:39:36 +0300

    Update 19.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -389,7 +389,7 @@ namespace FunctionsPartTwo
          int[][] arrays = new int[5][];
          // Заполнение массивов
          arrays[0] = new[] { 5, 7, 9 };
-         arrays[1] = new[] { 3, 8 };
+         arrays[1] = new[] { 3, 8, 11, 15 };
          arrays[2] = new[] { 2, 9, 18 };
          arrays[3] = new[] { 1, 3, 7, 8 };
          arrays[4] = new[] { 12, 17 };
