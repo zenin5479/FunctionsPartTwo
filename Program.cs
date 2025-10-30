7115df16b9bf84ba3669b8dbaefb6c193277c192
zenin5479 <zenin5479@gmail.com>
2025-10-30 09:28:19 +0300

    Update 20.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -413,9 +413,7 @@ namespace FunctionsPartTwo
          // Вывод результата
          Console.WriteLine("Массив с наибольшей суммой элементов:");
          Console.WriteLine($"[{string.Join(", ", stockcollection)}]");
-
          Console.WriteLine("Сумма элементов: {0}", limitaggregate);
-
       }
    }
 }
