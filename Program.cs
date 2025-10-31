b099d977f8e6d16ee7f6bd5b46705b2735b8808c
zenin5479 <zenin5479@gmail.com>
2025-10-31 08:51:17 +0300

    Update 22.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -422,6 +422,7 @@ namespace FunctionsPartTwo
          arrayarrays[2] = new[] { 1, 2, 3, 4 };
          int x = 0;
          int summa = 0;
+         Console.WriteLine("Массив массивов размером [{0}]", arrayarrays.Length);
          while (x < arrayarrays.Length)
          {
             Console.WriteLine("Массив с индексом [{0}] массива массивов размером [{1}], количество элементов: [{2}]", x, arrayarrays.Length, arrayarrays[x].Length);
