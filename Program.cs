d5a8524a05bda03044d9ac030b247c8ed1f0a811
zenin5479 <zenin5479@gmail.com>
2025-11-01 10:08:35 +0300

    Update 25.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -467,7 +467,6 @@ namespace FunctionsPartTwo
 
          void MaxSumArray(int[] sum)
          {
-            // Поиск максимального элемента строки (без флагов bool)
             // Cчитаем, что максимум - это первый элемент строки
             int max = sum[0];
             int column = 0;
