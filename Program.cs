eb4d310c7be0ac511aa28008ce85004e7cc8ac06
zenin5479 <zenin5479@gmail.com>
2025-11-04 16:32:07 +0300

    Update 31.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -533,21 +533,12 @@ namespace FunctionsPartTwo
 
          int indexminarray = MinIndexArray(countelements, maxsum);
 
-         OutputtingArray(arrayarrays[indexminarray]);
+         
 
          Console.WriteLine("Массив с наибольшей суммой элементов и наименьшим индексом массива массивов: [{0}][{1}]",
             indexminarray, arrayarrays[indexminarray].Length);
-         Console.WriteLine("Массив [{0}][{1}]", indexminarray, arrayarrays[indexminarray].Length);
-         int с = 0;
-         while (с < arrayarrays[indexminarray].Length)
-         {
-            amount += arrayarrays[indexminarray][с];
-            Console.Write("{0} ", arrayarrays[indexminarray][с]);
-            с++;
-         }
-
-         Console.WriteLine();
-         Console.WriteLine("Сумма элементов: {0}", amount);
+         OutputtingArray(arrayarrays[indexminarray]);
+         Console.WriteLine("Сумма элементов: {0}", countelements[indexminarray]);
 
 
 
